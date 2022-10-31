c81f9033d2815f12437d862e52cd7fc26a8f5aeb
Ubuntu <yuxuan.ye@outlook.com>
2022-10-31 19:56:11 +0000

    add bash script

--- marking.sh
+++ marking.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition gpu_short
+#SBATCH --job-name=eval_0
+#SBATCH --time=6:0:0
+#SBATCH --mem=16384M
+
+cd "${SLURM_SUBMIT_DIR}"
+
 source venv/bin/activate
 python -u marker.py --summary results/epoch_0/predictions.json --reference results/references.json --source results/source.json --rouge --factcc
